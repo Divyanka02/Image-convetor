de001c211661567aa280c3347270aeb90f6a5ee8
Divyanka02 <85795778+Divyanka02@users.noreply.github.com>
2021-08-03 14:23:06 +0530

    Update download.php

--- img-convetor/download.php
+++ img-convetor/download.php
@@ -22,33 +22,5 @@ fclose($fp);
 
 
 
-/*header("Content-Description: File Transfer"); 
-header("Content-Type: application/download");
-//header("Content-Type: application/octet-stream"); 
-header("Content-Disposition: attachment; filename=\"".urlencode($file_name)."\""); 
 
-readfile ($file_name);
-exit(); 
-*/
-
-
-
-
-
-
-
-
- /*$file_name = "files/";
- if(isset($_GET['file'])){
-     $file_name .= $_GET['file'];
- } 
- if(file_exists($file_name)){
-     $mime_type = mime_content_type($file_name);
-     header("content-type:".$mime_type);
-     header('Content-disposition:attachment; filename="'.$file_name.'"');
-     readfile($file_name);
- }
- else{
-     echo "file not found";
- }*/
 ?>
